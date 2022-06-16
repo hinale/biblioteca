82da6b9fbc1707bd9091af1d2f8aa893ece81247
Letícia Amaral <leticiaamarallara@hotmail.com>
2022-06-16 20:25:14 -0300

    mensagem de alerta

--- biblioteca_virtual/livros/urls.py
+++ biblioteca_virtual/livros/urls.py
@@ -2,9 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.login, name='login'),
     path('plataforma/', views.plataforma, name='plataforma'),
     path('cadastro_livro/', views.cadastro_livro, name='cadastro_livro'),
+    path('alerta/', views.alerta, name='alerta'),
 ]
--- biblioteca_virtual/livros/views.py
+++ biblioteca_virtual/livros/views.py
@@ -6,60 +6,65 @@ from django.contrib.auth import authenticate, login as login_django
 from .models import Livro
 
 # Create your views here.
-def index(request):
-  return render(request, 'livros/index.html')
+
 
 def cadastro(request):
-  if request.method == 'GET':
-    return render(request, 'livros/cadastro.html')
-  else:
-    username = request.POST.get('username')
-    email = request.POST.get('email')
-    senha = request.POST.get('senha')
+    if request.method == 'GET':
+        return render(request, 'livros/cadastro.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
 
-    user = User.objects.filter(username=username).first()
+        user = User.objects.filter(username=username).first()
 
-    if user:
-      return HttpResponse('Usuário já existe')
-    user = User.objects.create_user(
-        username=username, email=email, password=senha)
-    user.save()
-    return redirect('plataforma')
+        if user:
+            return HttpResponse('Usuário já existe')
+        user = User.objects.create_user(
+            username=username, email=email, password=senha)
+        user.save()
+        return redirect('plataforma')
+
 
 def login(request):
-  if request.method == 'GET':
-    return render(request, 'livros/login.html')
-  else:
-    username = request.POST.get('username')
-    senha = request.POST.get('senha')
-
-    user = authenticate(username=username, password=senha)
-    if(user):
-      login_django(request, user)
-      return redirect('plataforma')
+    if request.method == 'GET':
+        return render(request, 'livros/login.html')
     else:
-      return HttpResponse('Usuário ou senha inválidos')
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=username, password=senha)
+        if(user):
+            login_django(request, user)
+            return redirect('plataforma')
+        else:
+            return HttpResponse('Usuário ou senha inválidos')
 
 
 def plataforma(request):
-  livros = Livro.objects.all()
-  if request.user.is_authenticated:
-    return render(request, 'livros/plataforma.html', {'livros': livros})
-  return HttpResponse('Você precisa estar logado')
+    livros = Livro.objects.all()
+    if request.user.is_authenticated:
+        return render(request, 'livros/plataforma.html', {'livros': livros})
+    return redirect('alerta')
+
 
 def cadastro_livro(request):
-  if request.method == 'GET':
-    return render(request, 'livros/plataforma.html')
-  else:
-    titulo = request.POST.get('titulo')
-    autor = request.POST.get('autor')
-    ano = request.POST.get('ano')
+    if request.method == 'GET':
+        return render(request, 'livros/plataforma.html')
+    else:
+        titulo = request.POST.get('titulo')
+        autor = request.POST.get('autor')
+        ano = request.POST.get('ano')
 
-    livro = Livro.objects.filter(titulo=titulo).first()
+        livro = Livro.objects.filter(titulo=titulo).first()
 
-    if livro:
-      return HttpResponse('Livro já existe')
-    livro = Livro.objects.create(
-        titulo=titulo, autor=autor, ano=ano)
-    livro.save()
-    return redirect('plataforma')
+        if livro:
+            return HttpResponse('Livro já existe')
+        livro = Livro.objects.create(
+            titulo=titulo, autor=autor, ano=ano)
+        livro.save()
+        return redirect('plataforma')
+
+
+def alerta(request):
+    return render(request, 'livros/alerta.html')
